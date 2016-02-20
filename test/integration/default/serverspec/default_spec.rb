7a38f3533df33fd93d7bf59264c3d06babd7f5ec
Scott G <scottg489@gmail.com>
2016-02-19 22:02:06 -0500

    Add basic serverspec integration tests.

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,9 +1,26 @@
 require 'spec_helper'
 
-describe 'diff-info-service::default' do
-  # Serverspec examples can be found at
-  # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
-  end
+describe command('docker -v') do
+  its(:exit_status) { should eq 0 }
+  its(:stdout) { should match /Docker version.*/ }
+end
+
+describe service('docker') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe docker_image('diff-info-service') do
+  it { should exist }
+  its(['State.Status']) { should eq 'running' }
+  its(['State.Running']) { should eq true }
+end
+
+describe port(8080) do
+  it { should be_listening }
+end
+
+describe command('curl localhost:8080') do
+  its(:stdout) { should eq '{"code":404,"message":"HTTP 404 Not Found"}' }
+  its(:exit_status) { should eq 0 }
 end
