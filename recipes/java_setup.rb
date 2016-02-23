bc06b38e4c06899fc82bdfdc2acae3d9d0fd2125
Scott G <scottg489@gmail.com>
2016-02-23 00:06:32 -0500

    Try service again to start jar.

--- recipes/java_setup.rb
+++ recipes/java_setup.rb
@@ -12,11 +12,11 @@ remote_file '/opt/diff-info-service/diff-info-service.jar' do
   source 'https://oss.jfrog.org/artifactory/repo/com/github/scottg489/diff-info-service/%5BRELEASE%5D/diff-info-service-%5BRELEASE%5D-capsule.jar'
 end
 
-#poise_service 'diff-info-service' do
-#  command 'java -jar diff-info-service.jar server'
-#  directory '/opt/diff-info-service'
-#end
-
-execute 'runit' do
-  command 'java -jar /opt/diff-info-service.jar server &'
+poise_service 'diff-info-service' do
+  command 'java -jar diff-info-service.jar server'
+  directory '/opt/diff-info-service'
 end
+
+#execute 'runit' do
+#  command 'java -jar /opt/diff-info-service.jar server &'
+#end
