81582741f918a378b2809100c2313bf44fdbbd9b
Scott G <scottg489@gmail.com>
2016-02-25 18:49:20 -0500

    Revert back to docker for kitchen.

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,6 +4,6 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-include_recipe 'apt::default'
+#include_recipe 'apt::default'
 include_recipe 'diff-info-service::docker_setup'
 #include_recipe 'diff-info-service::java_setup'
