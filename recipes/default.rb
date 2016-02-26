7a145a1404cfd30b9596a2227a5c8682f7259581
Scott G <scottg489@gmail.com>
2016-02-25 20:38:40 -0500

    Set CI environment var in kitchen config as an attribute so it's visible to the provisioned machine.

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,10 +6,12 @@
 
 #include_recipe 'apt::default'
 # TODO: Since CI currently can't run the docker recipe for unknown reasons this is necessary
-if ENV['CI'] == 'true'
+if node['diff-info-service']['CI'] == 'true'
     puts 'CI ENV DETECTED, USING JAVA SETUP RECIPE'
+    puts "CI is: '#{node['diff-info-service']['CI']}'"
     include_recipe 'diff-info-service::java_setup'
 else
     puts 'NON-CI ENV DETECTED, USING DOCKER SETUP RECIPE'
+    puts "CI is: '#{node['diff-info-service']['CI']}'"
     include_recipe 'diff-info-service::docker_setup'
 end
