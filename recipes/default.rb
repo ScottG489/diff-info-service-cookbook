16a24c1299b4a1e6653886072f62c9993386d7ce
Scott G <scottg489@gmail.com>
2016-02-25 19:12:37 -0500

    Run java setup recipe only if on CI.

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,5 +5,11 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
 #include_recipe 'apt::default'
-include_recipe 'diff-info-service::docker_setup'
-#include_recipe 'diff-info-service::java_setup'
+# TODO: Since CI currently can't run the docker recipe for unknown reasons this is necessary
+if ENV['CI'] == 'true'
+    puts 'CI ENV DETECTED, USING JAVA SETUP RECIPE'
+    include_recipe 'diff-info-service::java_setup'
+else
+    puts 'NON-CI ENV DETECTED, USING DOCKER SETUP RECIPE'
+    include_recipe 'diff-info-service::docker_setup'
+end
