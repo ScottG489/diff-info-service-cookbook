a007b7e13fa3fdf990831aeee4ec78e9c2065c7d
Scott G <scottg489@gmail.com>
2016-02-28 10:25:23 -0500

    Create a .kitchen.local.yml file for use on CI. Will just have to renamed on the CI environment as we don't want it to be used by default.

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,13 +5,4 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
 #include_recipe 'apt::default'
-# TODO: Since CI currently can't run the docker recipe for unknown reasons this is necessary
-if node['diff-info-service']['CI'] == 'true'
-    puts 'CI ENV DETECTED, USING JAVA SETUP RECIPE'
-    puts "CI is: '#{node['diff-info-service']['CI']}'"
-    include_recipe 'diff-info-service::java_setup'
-else
-    puts 'NON-CI ENV DETECTED, USING DOCKER SETUP RECIPE'
-    puts "CI is: '#{node['diff-info-service']['CI']}'"
-    include_recipe 'diff-info-service::docker_setup'
-end
+include_recipe 'diff-info-service::docker_setup'
