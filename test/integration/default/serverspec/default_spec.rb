83fb45b7c5643a0adf65d8ce227ac33172544910
Scott G <scottg489@gmail.com>
2016-02-29 23:08:41 -0500

    Move expected and actual strings out of test and into resource files. Parse and compare JSON appropriately for expected and actual values.

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'json'
 
 # TODO: These are no longer relevant without docker. Move them to a separate file to save but ignore test?
 =begin
@@ -36,74 +37,25 @@ describe command('curl localhost:8080/info') do
 end
 
 
-# TODO: This is all pretty terrible but I'll keep it until I can figure out how to do it appropriately.
-# TODO: How can I read these from a file instead of having the string literal here?
-# TODO: How can I read a JSON object from the result of the curl command? The fields are in different order so comparing
-# TODO:         against the expected result exactly isn't the way to go.
-# TODO: Additionally this isn't even parsing correctly as is.
-multiline_patch = %q{diff --git a/.gitignore b/.gitignore\r\nnew file mode 100644\r\nindex 0000000..d490e8e\r\n--- /dev/null\r\n+++ b/.gitignore\r\n@@ -0,0 +1,19 @@\r\n+.*swp\r\n+.coverage\r\n+.ropeproject\r\n+\r\n+src/tasks.dat\r\n+\r\n+src/examples/taskfile\r\n+src/examples/keyfile\r\n+src/examples/taskdb\r\n+src/examples/tasks.dat\r\n+src/examples/*.pyc\r\n+\r\n+src/task/.ropeproject/\r\n+src/task/*.pyc\r\n+\r\n+src/test/.ropeproject/\r\n+src/test/taskdb\r\n+src/test/tasks.dat\r\n+src/test/*.pyc\r\ndiff --git a/task.py b/task.py\r\ndeleted file mode 100644\r\nindex 70e053b..0000000\r\n--- a/task.py\r\n+++ /dev/null\r\n@@ -1,15 +0,0 @@\r\n-import textwrap\r\n-class Task():\r\n-\tdef __init__(self, title='', notes = '', priority = int(), tags = []):\r\n-\t\tself.title = title\r\n-\t\tself.notes = notes\r\n-\t\tself.priority = priority\r\n-\t\tself.tags = tags\r\n-\r\n-\tdef __str__(self):\r\n-\t\treturn textwrap.dedent('''\\\r\n-\t\t\tTitle: %(title)s\r\n-\t\t\tNotes: %(notes)s''') % {\r\n-\t\t\t\t'title': self.title,\r\n-\t\t\t'notes': self.notes\r\n-\t\t}\r\ndiff --git a/a b/a\r\nindex 5c31be7..45cfaf4 100644\r\n--- a/a\r\n+++ b/a\r\n@@ -1,9 +1,9 @@\r\n asdf\r\n asdf\r\n-asfd\r\n fasd\r\n afsd\r\n-asfdfad\r\n+asdf\r\n+fdfad\r\n sfad\r\n sfa\r\n sd}
+multiline_patch = File.open("/tmp/verifier/suites/serverspec/resources/multi.patch", "rb").read
+expected_result = JSON.parse(File.open("/tmp/verifier/suites/serverspec/resources/expected.json", "rb").read)
 
-# TODO: This is what the result would look like for the most part but the field ordering might be different so I need
-# TODO:         to figure out how to parse the resulting JSON.
-
-expected_result = %q{[ {
-  "rawDiff" : "diff --git a/.gitignore b/.gitignore\r\nnew file mode 100644\r\nindex 0000000..d490e8e\r\n--- /dev/null\r\n+++ b/.gitignore\r\n@@ -0,0 +1,19 @@\r\n+.*swp\r\n+.coverage\r\n+.ropeproject\r\n+\r\n+src/tasks.dat\r\n+\r\n+src/examples/taskfile\r\n+src/examples/keyfile\r\n+src/examples/taskdb\r\n+src/examples/tasks.dat\r\n+src/examples/*.pyc\r\n+\r\n+src/task/.ropeproject/\r\n+src/task/*.pyc\r\n+\r\n+src/test/.ropeproject/\r\n+src/test/taskdb\r\n+src/test/tasks.dat\r\n+src/test/*.pyc\r\n",
-  "fromFile" : "/dev/null",
-  "toFile" : ".gitignore",
-  "fileStatus" : "Added",
-  "mode" : "100644",
-  "oldMode" : null,
-  "similarityIndex" : null,
-  "checksumBefore" : "0000000",
-  "checksumAfter" : "d490e8e",
-  "binary" : false,
-  "disimilarityIndex" : null,
-  "addedFile" : true,
-  "copied" : false,
-  "removedFile" : false,
-  "renamed" : false,
-  "modifiedFile" : false
-}, {
-  "rawDiff" : "diff --git a/task.py b/task.py\r\ndeleted file mode 100644\r\nindex 70e053b..0000000\r\n--- a/task.py\r\n+++ /dev/null\r\n@@ -1,15 +0,0 @@\r\n-import textwrap\r\n-class Task():\r\n-\tdef __init__(self, title='', notes = '', priority = int(), tags = []):\r\n-\t\tself.title = title\r\n-\t\tself.notes = notes\r\n-\t\tself.priority = priority\r\n-\t\tself.tags = tags\r\n-\r\n-\tdef __str__(self):\r\n-\t\treturn textwrap.dedent('''\\\r\n-\t\t\tTitle: %(title)s\r\n-\t\t\tNotes: %(notes)s''') % {\r\n-\t\t\t\t'title': self.title,\r\n-\t\t\t'notes': self.notes\r\n-\t\t}\r\n",
-  "fromFile" : "task.py",
-  "toFile" : "/dev/null",
-  "fileStatus" : "Removed",
-  "mode" : "100644",
-  "oldMode" : null,
-  "similarityIndex" : null,
-  "checksumBefore" : "70e053b",
-  "checksumAfter" : "0000000",
-  "binary" : false,
-  "disimilarityIndex" : null,
-  "addedFile" : false,
-  "copied" : false,
-  "removedFile" : true,
-  "renamed" : false,
-  "modifiedFile" : false
-}, {
-  "rawDiff" : "diff --git a/a b/a\r\nindex 5c31be7..45cfaf4 100644\r\n--- a/a\r\n+++ b/a\r\n@@ -1,9 +1,9 @@\r\n asdf\r\n asdf\r\n-asfd\r\n fasd\r\n afsd\r\n-asfdfad\r\n+asdf\r\n+fdfad\r\n sfad\r\n sfa\r\n sd",
-  "fromFile" : "a",
-  "toFile" : "a",
-  "fileStatus" : "Modified",
-  "mode" : "100644",
-  "oldMode" : null,
-  "similarityIndex" : null,
-  "checksumBefore" : "5c31be7",
-  "checksumAfter" : "45cfaf4",
-  "binary" : false,
-  "disimilarityIndex" : null,
-  "addedFile" : false,
-  "copied" : false,
-  "removedFile" : false,
-  "renamed" : false,
-  "modifiedFile" : true
-} ]}
-
-describe command("curl -X GET -H 'Content-Type:text/plain' --data \"#{multiline_patch}\" localhost:8080/info") do
-  # TODO: Don't compare against exact result but parse JSON into an object and check field values.
-  #its(:stdout) { should eq expected_result }
-  # TODO: This will have to do as a check for now
-  its(:stdout) { should match /rawDiff/ }
+describe command("curl -X GET -H 'Content-Type:text/plain' --data-binary \"#{multiline_patch}\" localhost:8080/info") do
+  it "has expected values" do
+      actual_result = JSON.parse(subject.stdout)
+      expect(actual_result.length).to eq(3)
+      # TODO: This may be a bug, it's returning the rawDiff with \n always even when in this case the input is \r\n
+      # TODO: What behavior do we want?
+      #compare(actual_result, expected_result, 'rawDiff')
+      compare(actual_result, expected_result, 'fromFile')
+      compare(actual_result, expected_result, 'toFile')
+      compare(actual_result, expected_result, 'mode')
+  end
   its(:exit_status) { should eq 0 }
 end
 
+def compare(actual_result, expected_result, field)
+  expect(actual_result[0][field]).to eq(expected_result[0][field])
+  expect(actual_result[1][field]).to eq(expected_result[1][field])
+  expect(actual_result[2][field]).to eq(expected_result[2][field])
+end
